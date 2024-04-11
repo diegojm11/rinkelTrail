ed40b4900f081e35d1425377c1d3e7c1162349a7
diegojm11 <diegojm.11@hotmail.com>
2024-04-11 00:59:30 -0300

    fixed1

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
   
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 120) {
+      if (window.scrollY > 200) {
         setBannerScrolled(true);
       } else {
         setBannerScrolled(false);
