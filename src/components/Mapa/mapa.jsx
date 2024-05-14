d13bb4b4fe5be80191b3555710e835d6524e09be
diegojm11 <diegojm.11@hotmail.com>
2024-05-14 20:58:00 -0300

    mapa2

--- src/components/Mapa/mapa.jsx
+++ src/components/Mapa/mapa.jsx
@@ -21,6 +21,8 @@ const Mapa = () => {
            <span className='punto5'></span>
            <span className='punto5-'></span>
             <div className='tituloMapa'>
+
+                <h2>C  O  B  E  R  T  U  R  A </h2>
                 <h3>
                     Rinkel trail cuenta con una extensa flota de unidades propias que combinada con nuestra exclusiva red de prestadores asegura la cobertura en toda la argentina y paises limitrofes.
                 </h3>
