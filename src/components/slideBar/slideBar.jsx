658ae3f97633c36d198048b1a8912fd16dd48ec3
diegojm11 <diegojm.11@hotmail.com>
2024-05-15 23:16:34 -0300

    .

--- src/components/slideBar/slideBar.jsx
+++ src/components/slideBar/slideBar.jsx
@@ -2,13 +2,17 @@ import Amca from '../../assets/img/logos/amca1.png'
 import Ausa from '../../assets/img/logos/Ausa1.png'
 import Caledonia from '../../assets/img/logos/caledonia1.png'
 import Escudo from '../../assets/img/logos/escudo1.png'
-import FederalSeguros from '../../assets/img/logos/federalseguros1.jpg'
 import Metropol from '../../assets/img/logos/metropol1.png'
 import NacionSeguros from '../../assets/img/logos/Nacion-seguros1.png'
 import Orbis from '../../assets/img/logos/orbis1.png'
 import OrganizacionPalermo from '../../assets/img/logos/organizacionpalermo1.png'
 import Patronal from '../../assets/img/logos/patronal1.png'
 import Providencia from '../../assets/img/logos/providencia1.png'
+import OrgCentro from '../../assets/img/logos/orgcentro.png'
+import TpcSeguros from '../../assets/img/logos/tpcseguros.png'
+import Prof from '../../assets/img/logos/prof.png'
+import Corredores from '../../assets/img/logos/corredores.png'
+
 import './slideBar.css'
 
 const SlideBar = () => {
@@ -28,25 +32,33 @@ const SlideBar = () => {
                             <li className="marcas-logo" > <img src={Ausa}  alt="" /> </li>
                             <li className="marcas-logo" > <img src={Caledonia}  alt="" /> </li>
                             <li className="marcas-logo" > <img src={Escudo}  alt="" /> </li>
-                            <li className="marcas-logo" > <img src={FederalSeguros}  alt="" /> </li>
                             <li className="marcas-logo" > <img src={Metropol}  alt="" /> </li>
                             <li className="marcas-logo" > <img src={NacionSeguros}  alt="" /> </li>
                             <li className="marcas-logo" > <img src={Orbis}  alt="" /> </li>
                             <li className="marcas-logo" > <img src={OrganizacionPalermo}  alt="" /> </li>
                             <li className="marcas-logo" > <img src={Patronal}  alt="" /> </li>
                             <li className="marcas-logo" > <img src={Providencia}  alt="" /> </li>
+                            <li className="marcas-logo" > <img src={OrgCentro}  alt="" /> </li>
+                            <li className="marcas-logo" > <img src={TpcSeguros}  alt="" /> </li>
+                            <li className="marcas-logo" > <img src={Prof}  alt="" /> </li>
+                            <li className="marcas-logo" > <img src={Corredores}  alt="" /> </li>
+
 
                             <li className="marcas-logo" > <img src={Amca}  alt="" /> </li>
                             <li className="marcas-logo" > <img src={Ausa}  alt="" /> </li>
                             <li className="marcas-logo" > <img src={Caledonia}  alt="" /> </li>
                             <li className="marcas-logo" > <img src={Escudo}  alt="" /> </li>
-                            <li className="marcas-logo" > <img src={FederalSeguros}  alt="" /> </li>
                             <li className="marcas-logo" > <img src={Metropol}  alt="" /> </li>
                             <li className="marcas-logo" > <img src={NacionSeguros}  alt="" /> </li>
                             <li className="marcas-logo" > <img src={Orbis}  alt="" /> </li>
                             <li className="marcas-logo" > <img src={OrganizacionPalermo}  alt="" /> </li>
                             <li className="marcas-logo" > <img src={Patronal}  alt="" /> </li>
                             <li className="marcas-logo" > <img src={Providencia}  alt="" /> </li>
+                            <li className="marcas-logo" > <img src={OrgCentro}  alt="" /> </li>
+                            <li className="marcas-logo" > <img src={TpcSeguros}  alt="" /> </li>
+                            <li className="marcas-logo" > <img src={Prof}  alt="" /> </li>
+                            <li className="marcas-logo" > <img src={Corredores}  alt="" /> </li>
+
                         </ul>
                 </div>
         </div>
